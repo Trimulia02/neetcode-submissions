1a0ea04e377464c246465485b7bb60c65619b7e3
Tri Mulia <94356552+Trimulia02@users.noreply.github.com>
2026-06-20 15:36:27 +0700

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.js
@@ -0,0 +1,20 @@
+class Solution {
+    isValid(s) {
+        if(s.length < 2) return false
+        let stack =[]
+        for(let i=0; i< s.length;i++){
+            switch(s[i]){
+                case '[': 
+                case '(': 
+                case '{': stack.push(s[i])
+                break
+                case ']': if(stack.pop()!=='[') return false
+                break
+                case ')': if(stack.pop()!=='(') return false
+                break
+                case '}': if(stack.pop()!=='{') return false
+            }
+        }
+        return stack.length === 0
+    }
+}
