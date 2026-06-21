072b076ce15ebf62a05932cf829fee4c94a5e987
Tri Mulia <94356552+Trimulia02@users.noreply.github.com>
2026-06-21 13:12:45 +0700

    Add: median-of-two-sorted-arrays - submission-1

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-1.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number}
+     */
+    findMedianSortedArrays(nums1, nums2) {
+        let list = [...nums1,...nums2]
+        list.sort((a, b) => a - b);
+        let res=0
+        if(list.length % 2 ===0){
+            res = (list[(list.length / 2)-1] + list[(list.length / 2)])/2
+        }else{
+            res = list[Math.floor(list.length / 2)]
+        }return res
+    }
+}
