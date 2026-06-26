78e00f2254f1fd282b0d64f90136b02c5c6ac2aa
Tri Mulia <94356552+Trimulia02@users.noreply.github.com>
2026-06-26 22:08:35 +0700

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.js
@@ -0,0 +1,14 @@
+class Solution {
+    maxProfit(prices) {
+        let res=0
+        for(let i=0;i<prices.length;i++){
+            let temp = 0
+            for(let j=i+1; j<prices.length;j++){
+                temp = prices[j]-prices[i]
+                if(res < temp){
+                    res=temp
+                }
+            }
+        }return res
+    }
+}
