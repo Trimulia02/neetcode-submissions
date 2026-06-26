f5d3cfaa04e7ff0b4da7879eb5c6c31ff826829a
Tri Mulia <94356552+Trimulia02@users.noreply.github.com>
2026-06-26 21:17:43 +0700

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let l = 1
+        let r = Math.max(...piles)
+        console.log(r)
+        let res = r
+
+        while(l<=r){
+            let k = Math.floor((l+r)/2)
+            let hours =0
+            for(const p of piles){
+                hours+=Math.ceil(p/k)
+            }if(hours <=h){
+                res=k
+                r=k-1
+            }else{
+                l=k+1
+            }
+        }return res
+    }
+}
